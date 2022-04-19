0e5ab395485a35e6163a1bf48f5b1bf02dccd780
wandergithub <wandergonzalez0@hotmail.com>
2022-04-19 16:17:33 -0400

    refactore code to use classes

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
-let book = [];
-
 const givenName = document.querySelector('#name');
 const givenAuthor = document.querySelector('#author');
 const btnClass = document.querySelector('#Button');
 const listOfName = document.querySelector('#listOfName');
 
 function updateStorage() {
-  localStorage.setItem('books', JSON.stringify(book));
+  localStorage.setItem('books', JSON.stringify(booklist.book));
 }
-function removeBook(id, c = true) {
-  if (c === false) {
-    return;
+
+class BookList {
+  constructor (book) {
+    this.book = book;
   }
-  for (let i = 0; i < book.length; i += 1) {
-    const bookid = i;
-    if (book[bookid].id === Number(id)) {
-      book.splice(bookid, 1);
-      listOfName.removeChild(listOfName.querySelector(`[id='${id.toString()}']`));
-      updateStorage();
+  
+  removeBook(id, c = true) {
+    if (c === false) {
       return;
     }
+    for (let i = 0; i < this.book.length; i += 1) {
+      const bookid = i;
+      if (this.book[bookid].id === Number(id)) {
+        this.book.splice(bookid, 1);
+        listOfName.removeChild(listOfName.querySelector(`[id='${id.toString()}']`));
+        updateStorage();
+        return;
+      }
+    }
+  }
+  
+  addBook(obj, old = false) {
+    let author;
+    let name;
+    let id;
+    if (old === true) {
+      author = obj.author;
+      name = obj.title;
+      id = obj.id;
+    } else {
+      author = givenAuthor.value;
+      name = givenName.value;
+      id = obj;
+    }
+    const createAnHTMLList = `<li id="${id}"><span>${author}</span><br><span>${name}</span><br><button class="btn2"
+        onclick="booklist.removeBook('${id}')">Remove</button><hr>`;
+    listOfName.innerHTML += createAnHTMLList;
+    updateStorage();
   }
 }
 
-function addBook(obj, old = false) {
-  let author;
-  let name;
-  let id;
-  if (old === true) {
-    author = obj.author;
-    name = obj.title;
-    id = obj.id;
-  } else {
-    author = givenAuthor.value;
-    name = givenName.value;
-    id = obj;
-  }
-  const createAnHTMLList = `<li id="${id}"><span>${author}</span><br><span>${name}</span><br><button class="btn2"
-      onclick="removeBook('${id}')">Remove</button><hr>`;
-  listOfName.innerHTML += createAnHTMLList;
-  updateStorage();
-}
 btnClass.addEventListener('click', () => {
   const bookid = new Date().getTime();
-  book.push({ title: givenName.value, author: givenAuthor.value, id: bookid });
+  booklist.book.push({ title: givenName.value, author: givenAuthor.value, id: bookid });
   if (givenAuthor.length !== 0 && givenName.length !== 0) {
-    addBook(bookid);
+    booklist.addBook(bookid);
     givenName.value = '';
     givenAuthor.value = '';
   } else {
@@ -57,11 +63,12 @@ function getStorage() {
   if (localStorage.length === 0) {
     updateStorage();
   } else {
-    book = JSON.parse(localStorage.getItem('books'));
-    book.forEach((obj) => {
-      addBook(obj, true);
+    booklist.book = JSON.parse(localStorage.getItem('books'));
+    booklist.book.forEach((obj) => {
+      booklist.addBook(obj, true);
     });
   }
 }
-removeBook('As', false);
+let booklist = new BookList([]);;
+// removeBook('As', false);
 window.addEventListener('load', getStorage());
